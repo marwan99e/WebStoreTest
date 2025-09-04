fdf4c352426d965ae161d8c35cb53ed2c495b08f
marwan99e <marwanrawashdeh02@gmail.com>
2025-09-04 14:55:17 +0300

    update

---
+++ src/Utils/TestDataGenerator.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import java.util.Random;
+
+public class TestDataGenerator {
+	
+	
+	static Random rand = new Random();
+
+    public static String getRandomFirstName() {
+        String[] names = {"amira", "rogina", "dana", "mais", "dareen","eslam", "esraa", "rahaf", "rema"};
+        return names[rand.nextInt(names.length)];
+    }
+
+    public static String getRandomLastName() {
+        String[] names = {"alaa", "saif", "abduallah", "hamzeh", "marwan", "abedalrahman", "omar","yazan"};
+        return names[rand.nextInt(names.length)];
+    }
+
+    public static String getEmail(String first, String last) {
+        return first + last + rand.nextInt(7000) + "@gmail.com";
+    }
+
+    public static String getRandomUsername(String first, String last) {
+        return first + last + rand.nextInt(7000);
+    }
+
+}
